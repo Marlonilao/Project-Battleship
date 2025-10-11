3e31424c1979ed1a9c32eec5bf03bc3f8d4a298d
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2025-10-11 11:35:58 +0800

    chore: update ESLint config && deleted eslintignore file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,12 +4,13 @@ import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   {
-    files: ['**/*.{js,mjs,cjs}'],
+    files: ['src/**/*.{js,mjs,cjs}'],
+    ignores: ['dist/**', 'node_modules/**'],
     plugins: { js },
     extends: ['js/recommended'],
     languageOptions: { globals: { ...globals.browser, ...globals.node } },
     rules: {
-      qoutes: ['error', 'single'],
+      quotes: ['error', 'single'],
     },
   },
 ]);
