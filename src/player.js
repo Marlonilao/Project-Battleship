75c227488bad2c28d37f0f3f2140dff4fe9a74e2
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2025-10-15 09:08:21 +0800

    feat: Create Player Factory

---
+++ src/player.js
@@ -0,0 +1,15 @@
+function player(gameboard) {
+  const board = gameboard;
+  return {
+    board,
+  };
+}
+
+function botPlayer(gameboard) {
+  const board = gameboard;
+  return {
+    board,
+  };
+}
+
+export { player, botPlayer };
---
+++ src/player.test.js
@@ -0,0 +1,42 @@
+import { player, botPlayer } from './player';
+import { gameboard } from './gameboard';
+import { ships } from './ships';
+
+test('Player object should be defined', () => {
+  expect(player).toBeDefined();
+  expect(botPlayer).toBeDefined();
+});
+
+test('Each player should contain its own gameboard', () => {
+  const player1Board = gameboard();
+  const botBoard = gameboard();
+
+  const player1 = player(player1Board);
+  const bot1 = botPlayer(botBoard);
+
+  expect(player1.board.getBoard()).toEqual([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+
+  expect(bot1.board.getBoard()).toEqual([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+});
